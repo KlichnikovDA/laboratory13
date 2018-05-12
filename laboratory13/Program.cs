e341f871edda024dc757cf088d29f84e873b29ee
KlichnikovDA <spacerus99@gmail.com>
2018-05-12 23:46:50 +0500

    Реализовал класс TestCollections. Реализовал генерацию коллекций, поиск элементов с подсчетом затраченного времени и добавление в коллекцию новых элементов.

--- laboratory13/Program.cs
+++ laboratory13/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Librarium;
 
 namespace laboratory13
 {
@@ -10,6 +11,7 @@ namespace laboratory13
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
